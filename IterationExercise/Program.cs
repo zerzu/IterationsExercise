981cd96deb2c707ed929828f40ca148857e5fbe5
Eric Garner <ericgarner81@gmail.com>
2023-10-03 23:04:50 -0500

    completed exercise 1

---
+++ IterationExercise/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace IterationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 5; i++) //1
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //2
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //3
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0;i < 5; i++)  //4
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //5
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //6
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //7
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //8
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //9
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //10
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //11
+            {
+                Console.WriteLine($"{i}"); 
+            }
+
+            for (int i = 0; i <5; i++)  //12
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i <5; i++)  //13
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //14
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++) //15
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
